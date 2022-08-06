0e45dc330bb7821874a08fb79e0d972e7ca36f5c
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-06 10:38:43 -0300

    [feat] connection cancel at exit to avoid memory leakage

--- db/connection.go
+++ db/connection.go
@@ -16,7 +16,8 @@ func getConnection() (client *mongo.Client, ctx context.Context) {
 		log.Fatal(err)
 	}
 
-	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
 	err = client.Connect(ctx)
 	if err != nil {
