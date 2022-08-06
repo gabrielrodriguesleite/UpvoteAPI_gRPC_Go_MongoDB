12f445be614da466b55e9a903dbcb12be6d32026
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-06 17:10:25 -0300

    [feat] server grpc

---
+++ server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "upvote/grpc"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// fmt.Println("server on...")
+
+	// err := db.Create("k.cogabriel@gmail.com", 100)
+	// if err != nil {
+	// 	fmt.Println("um erro ocorreu:", err)
+	// }
+
+	// votes, err := db.Get()
+	// if err != nil {
+	// 	fmt.Println("um erro ocorreu:", err)
+	// }
+
+	// fmt.Println(votes)
+
+	// fmt.Println("server off.")
+
+	lis, err := net.Listen("tcp", "localhost:50005")
+	if err != nil {
+		log.Fatalf("Fail to listen to: %v", err)
+	}
+
+	// grpc server
+	s := grpc.NewServer()
+	pb.RegisterUpvoteServiceServer(s, &server{})
+
+	log.Println("Starting server, listen to port 50005.")
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("Fail to serve: %v", err)
+	}
+}
