6b010e6060228edf6f702f699123869142f5101f
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-06 17:11:23 -0300

    [feat] client grpc

---
+++ client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	pb "upvote/grpc"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	fmt.Println("Starting Client...")
+
+	cc, err := grpc.Dial("localhost:50005", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Could note connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := pb.NewUpvoteServiceClient(cc)
+
+	doUpvote(c)
+
+}
