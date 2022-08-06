06dee93c7f0a72c803ccf9e83d5a0926bb3b4d02
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-06 11:22:11 -0300

    [feat] server Get ok

--- server/server.go
+++ server/server.go
@@ -7,9 +7,18 @@ import (
 
 func main() {
 	fmt.Println("server on...")
+
 	err := db.Create("k.cogabriel@gmail.com", 100)
 	if err != nil {
 		fmt.Println("um erro ocorreu:", err)
 	}
+
+	votes, err := db.Get()
+	if err != nil {
+		fmt.Println("um erro ocorreu:", err)
+	}
+
+	fmt.Println(votes)
+
 	fmt.Println("server off.")
 }
