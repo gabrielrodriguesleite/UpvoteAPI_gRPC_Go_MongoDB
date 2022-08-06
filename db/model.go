ba6b82dbe85688b2d66b7b12c139163e0429836c
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-06 11:21:14 -0300

    [feat] model list implemented

--- db/model.go
+++ db/model.go
@@ -1,6 +1,11 @@
 package db
 
-import "context"
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func Insert(collection string, data any) error {
 
@@ -12,5 +17,23 @@ func Insert(collection string, data any) error {
 	_, err := c.InsertOne(context.Background(), data)
 
 	return err
+
+}
+
+func List(collection string) (list []Vote, err error) {
+
+	client, ctx := getConnection()
+	defer client.Disconnect(ctx)
+
+	c := client.Database("upvote").Collection(collection)
+
+	cur, err := c.Find(context.Background(), bson.D{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	cur.All(context.Background(), &list)
+
+	return
 
 }
