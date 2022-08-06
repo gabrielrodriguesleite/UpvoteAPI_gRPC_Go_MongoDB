3205267018818b52593df8db481d8439fb69f79a
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-06 17:09:57 -0300

    [feat] server Upvote service

--- server/server.go
+++ server/server.go
@@ -3,22 +3,38 @@ package main
 import (
 	"fmt"
 	"upvote/db"
+	pb "upvote/grpc"
 )
 
-func main() {
-	fmt.Println("server on...")
+type server struct {
+	pb.UnimplementedUpvoteServiceServer
+}
 
-	err := db.Create("k.cogabriel@gmail.com", 100)
+func (*server) Upvote(req *pb.UpvoteRequest, server pb.UpvoteService_UpvoteServer) error {
+
+	fmt.Printf("Upvote was invoked with: %v\n", req)
+
+	// votar
+	err := db.Create(req.GetVote().GetEmail(), int(req.GetVote().GetScore()))
 	if err != nil {
-		fmt.Println("um erro ocorreu:", err)
+		fmt.Println("Error at Create: ", err)
 	}
 
+	// responder
 	votes, err := db.Get()
 	if err != nil {
-		fmt.Println("um erro ocorreu:", err)
+		fmt.Println("Error at Get: ", err)
 	}
 
-	fmt.Println(votes)
+	// non concurrent stream
+	for _, x := range votes {
+		resp := pb.UpvoteResponse{
+			Vote: &pb.Vote{
+				Email: x.Email, Score: int32(x.Score),
+			},
+		}
+		server.Send(&resp)
+	}
 
-	fmt.Println("server off.")
+	return err
 }
